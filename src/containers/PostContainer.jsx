2966acef2069aec303cdb8f71672c5735081ab19
viveloper <ucsik1234@gmail.com>
2020-06-19 10:51:55 +0900

    improving UX

--- src/containers/PostContainer.jsx
+++ src/containers/PostContainer.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
 import Post from '../components/Post';
 import { useSelector, useDispatch } from 'react-redux';
-import { getPost, clearPost } from '../modules/posts';
+import { getPost } from '../modules/posts';
+import { reducerUtils } from '../lib/asyncUtils';
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial()
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPost(postId));
-    return () => dispatch(clearPost());
+    // return () => dispatch(clearPost());
   }, [postId, dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
